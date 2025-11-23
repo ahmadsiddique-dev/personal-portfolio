50fda241ec8160fc302b88cce49501317fdc3418
Ahmad Siddique <asshikrani66@gmail.com>
2025-11-23 12:34:17 +0500

    error fixing of CORS issue in production

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,18 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import "dotenv/config";
-
-import router from "./src/routes/user.routes.js";
+import cors from "cors"
+import "dotenv/config"
 
 const app = express();
+app.use(express.json())
+app.use(cors({
+  // origin: process.env.CLIENT_URL,
+  // methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,               
+}));
+app.use(cookieParser())
 
-app.use(express.json());
-app.use(cookieParser());
+import router from "./src/routes/user.routes.js"
+import cookieParser from "cookie-parser";
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173", // fallback for local dev
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
-app.use("/api/v1", router);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/v1", router)
+export default app;
